aea75fab6358a52eda9bdb85e9ee207598d4967f
moonoonoom <569232693@qq.com>
2021-03-11 21:39:25 +0800

    上传测试

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
+// 全局配置
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
---
+++ src/views/IDE.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <NavMenu/>
+        <el-button>11</el-button>
+    </div>
+</template>
+
+
+
+<script>
+
+import NavMenu from '../components/NavMenu'
+
+export default{
+    components:{
+        NavMenu
+    }
+}
+
+</script>
